37818392193e597ac2c1ac65e8e063df0bfadccc
Dmitriy Lvov <Dmitriy.Lvov@electroshield.ru>
2022-08-09 14:08:18 +0400

    autotests_fix_9

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.use(cardRouter);
 app.use(userRouter);
 
 // Страница 404
-app.get('*', (req, res) => {
-  res.status(404).send(JSON.stringify({ message: '404 Page not found' }));
+app.use((req, res) => {
+  res.status(404).send({ message: '404 Page not found' });
 });
 
 app.listen(PORT, () => {
