55fe609054f1e70d24112a2f7430e80a834c4f86
Dmitriy Lvov <Dmitriy.Lvov@electroshield.ru>
2022-08-10 08:20:30 +0400

    review_1

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const Card = require('../models/card');
 
 const ERROR_NOT_FOUND = 404;
@@ -40,93 +39,152 @@ module.exports.createCard = (req, res) => {
 // Удаление карточки
 module.exports.deleteCard = (req, res) => {
   const { cardId } = req.params;
-  // Валидация на тип данных ObjectID
-  if (!mongoose.Types.ObjectId.isValid(cardId)) {
-    res
-      .status(ERROR_WRONG_DATA)
-      .send({ message: `Wrong format. Data ${cardId} is not ObjectID type.` });
-  } else {
-    // Проверка наличия карточки в БД перед выполнением действий
-    Card.findById(cardId).then((card) => {
-      if (card) {
-        Card.findByIdAndRemove(cardId)
-          .then(() => {
-            res.send({ message: `Card with ID ${cardId} was deleted` });
-          })
-          .catch((err) => {
-            res.status(ERROR_ANOTHER).send({
-              message: `Error in card deletion process: ${err.message}`,
-            });
-          });
-      } else {
+  Card.findByIdAndRemove(cardId)
+    .then((card) => {
+      // Если объект по ID не найден
+      if (!card) {
         res
           .status(ERROR_NOT_FOUND)
           .send({ message: `Card with ID ${cardId} not found.` });
+      } else {
+        res.send({ message: `Card with ID ${cardId} was deleted` });
       }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
+        });
+      }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in card deletion process: ${err.message}`,
+      });
     });
-  }
 };
+
+// // Добавление лайка карточке
+// module.exports.likeCard = (req, res) => {
+//   // Проверка наличия карточки в БД перед выполнением действий
+//   const { cardId } = req.params;
+//   if (!mongoose.Types.ObjectId.isValid(cardId)) {
+//     res
+//       .status(ERROR_WRONG_DATA)
+//       .send({ message: `Wrong format. Data ${cardId} is not ObjectID type.` });
+//   } else {
+//     Card.findById(cardId).then((card) => {
+//       if (card) {
+//         Card.findByIdAndUpdate(
+//           cardId,
+//           { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+//           { new: true },
+//         )
+//           .then((likedCard) => {
+//             res.send(likedCard);
+//           })
+//           .catch((err) => {
+//             res.status(ERROR_ANOTHER).send({
+//               message: `Error in like add process: ${err.message}`,
+//             });
+//           });
+//       } else {
+//         res
+//           .status(ERROR_NOT_FOUND)
+//           .send({ message: `Card with ID ${cardId} not found.` });
+//       }
+//     });
+//   }
+// };
 
 // Добавление лайка карточке
 module.exports.likeCard = (req, res) => {
-  // Проверка наличия карточки в БД перед выполнением действий
   const { cardId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(cardId)) {
-    res
-      .status(ERROR_WRONG_DATA)
-      .send({ message: `Wrong format. Data ${cardId} is not ObjectID type.` });
-  } else {
-    Card.findById(cardId).then((card) => {
-      if (card) {
-        Card.findByIdAndUpdate(
-          cardId,
-          { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-          { new: true },
-        )
-          .then((likedCard) => {
-            res.send(likedCard);
-          })
-          .catch((err) => {
-            res.status(ERROR_ANOTHER).send({
-              message: `Error in like add process: ${err.message}`,
-            });
-          });
-      } else {
+  Card.findByIdAndUpdate(
+    cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      // Если объект по ID не найден
+      if (!card) {
         res
           .status(ERROR_NOT_FOUND)
           .send({ message: `Card with ID ${cardId} not found.` });
+      } else {
+        res.send(card);
       }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
+        });
+      }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in like add process: ${err.message}`,
+      });
     });
-  }
 };
 
 // Удаление лайка карточки
+// module.exports.dislikeCard = (req, res) => {
+//   // Проверка наличия карточки в БД перед выполнением действий
+//   const { cardId } = req.params;
+//   if (!mongoose.Types.ObjectId.isValid(cardId)) {
+//     res
+//       .status(ERROR_WRONG_DATA)
+//       .send({ message: `Wrong format. Data ${cardId} is not ObjectID type.` });
+//   } else {
+//     Card.findById(cardId).then((card) => {
+//       if (card) {
+//         Card.findByIdAndUpdate(
+//           cardId,
+//           { $pull: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+//           { new: true },
+//         )
+//           .then((dislikedCard) => res.send(dislikedCard))
+//           .catch((err) => {
+//             res.status(ERROR_ANOTHER).send({
+//               message: `Error with dislike process: ${err.message}`,
+//             });
+//           });
+//       } else {
+//         res
+//           .status(ERROR_NOT_FOUND)
+//           .send({ message: `Card with ID ${cardId} not found.` });
+//       }
+//     });
+//   }
+// };
+
+// Удаление лайка карточке
 module.exports.dislikeCard = (req, res) => {
-  // Проверка наличия карточки в БД перед выполнением действий
   const { cardId } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(cardId)) {
-    res
-      .status(ERROR_WRONG_DATA)
-      .send({ message: `Wrong format. Data ${cardId} is not ObjectID type.` });
-  } else {
-    Card.findById(cardId).then((card) => {
-      if (card) {
-        Card.findByIdAndUpdate(
-          cardId,
-          { $pull: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-          { new: true },
-        )
-          .then((dislikedCard) => res.send(dislikedCard))
-          .catch((err) => {
-            res.status(ERROR_ANOTHER).send({
-              message: `Error with dislike process: ${err.message}`,
-            });
-          });
-      } else {
+  Card.findByIdAndUpdate(
+    cardId,
+    { $pull: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      // Если объект по ID не найден
+      if (!card) {
         res
           .status(ERROR_NOT_FOUND)
           .send({ message: `Card with ID ${cardId} not found.` });
+      } else {
+        res.send(card);
       }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
+        });
+      }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in like add process: ${err.message}`,
+      });
     });
-  }
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,3 @@
-const mongoose = require('mongoose');
 const User = require('../models/user');
 
 const ERROR_NOT_FOUND = 404;
@@ -17,28 +16,28 @@ module.exports.getAllUsers = (req, res) => {
 
 module.exports.getUserById = (req, res) => {
   const { userid } = req.params;
-  if (!mongoose.Types.ObjectId.isValid(userid)) {
-    res
-      .status(ERROR_WRONG_DATA)
-      .send({ message: `Wrong format. Data ${userid} is not ObjectID type.` });
-  } else {
-    // Поиск пользователя в mongoDB
-    User.findById(userid)
-      .then((user) => {
-        if (user) {
-          res.send(user);
-        } else {
-          res.status(ERROR_NOT_FOUND).send({
-            message: `User with ID ${userid} not found`,
-          });
-        }
-      })
-      .catch((err) => {
-        res.status(ERROR_ANOTHER).send({
-          message: `Error on server: ${err.message}`,
+  User.findById(userid)
+    .then((user) => {
+      // Если объект по ID не найден
+      if (!user) {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: `User with ID ${userid} not found.` });
+      } else {
+        res.send({ message: `User with ID ${userid} was deleted` });
+      }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
         });
+      }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in get user by id process: ${err.message}`,
       });
-  }
+    });
 };
 
 module.exports.createUser = (req, res) => {
@@ -60,56 +59,116 @@ module.exports.createUser = (req, res) => {
     });
 };
 
+// module.exports.updateUserInfo = (req, res) => {
+//   const { name, about } = req.body;
+//   const newUser = new User({ name, about, avatar: 'doesnt matter' });
+//   const validationResult = newUser.validateSync();
+//   if (validationResult) {
+//     res
+//       .status(ERROR_WRONG_DATA)
+//       .send({ message: 'Wrong data for "update user info" process' });
+//   } else {
+//     User.findById(req.user._id).then((user) => {
+//       if (user) {
+//         User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+//           .then((result) => {
+//             res.send(result);
+//           })
+//           .catch((err) => {
+//             res.status(ERROR_ANOTHER).send({
+//               message: `Error on server: ${err.message}`,
+//             });
+//           });
+//       } else {
+//         res.status(ERROR_NOT_FOUND).send({
+//           message: `User with Id ${req.user._id} not found`,
+//         });
+//       }
+//     });
+//   }
+// };
+
 module.exports.updateUserInfo = (req, res) => {
   const { name, about } = req.body;
-  const newUser = new User({ name, about, avatar: 'doesnt matter' });
-  const validationResult = newUser.validateSync();
-  if (validationResult) {
-    res
-      .status(ERROR_WRONG_DATA)
-      .send({ message: 'Wrong data for "update user info" process' });
-  } else {
-    User.findById(req.user._id).then((user) => {
-      if (user) {
-        User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
-          .then((result) => {
-            res.send(result);
-          })
-          .catch((err) => {
-            res.status(ERROR_ANOTHER).send({
-              message: `Error on server: ${err.message}`,
-            });
-          });
+  User.findByIdAndUpdate(
+    req.user._id,
+    { name, about },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      // Если объект по ID не найден
+      if (!user) {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: `User with ID ${req.user._id} not found.` });
       } else {
-        res.status(ERROR_NOT_FOUND).send({
-          message: `User with Id ${req.user._id} not found`,
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
         });
       }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in update user info process: ${err.message}`,
+      });
     });
-  }
 };
+
+// module.exports.updateAvatar = (req, res) => {
+//   const { avatar } = req.body;
+//   User.findById(req.user._id).then((user) => {
+//     if (user) {
+//       User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+//         .then((result) => res.send(result))
+//         .catch((err) => {
+//           if (err.name === 'ValidationError') {
+//             res
+//               .status(ERROR_WRONG_DATA)
+//               .send({ message: 'Wrong data for "update user avatar" process' });
+//           } else {
+//             res.status(ERROR_ANOTHER).send({
+//               message: `Error on server: ${err.message}`,
+//             });
+//           }
+//         });
+//     } else {
+//       res.status(ERROR_NOT_FOUND).send({
+//         message: `User with Id ${req.user._id} not found`,
+//       });
+//     }
+//   });
+// };
 
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findById(req.user._id).then((user) => {
-    if (user) {
-      User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-        .then((result) => res.send(result))
-        .catch((err) => {
-          if (err.name === 'ValidationError') {
-            res
-              .status(ERROR_WRONG_DATA)
-              .send({ message: 'Wrong data for "update user avatar" process' });
-          } else {
-            res.status(ERROR_ANOTHER).send({
-              message: `Error on server: ${err.message}`,
-            });
-          }
+  User.findByIdAndUpdate(
+    req.user._id,
+    { avatar },
+    { new: true, runValidators: true },
+  )
+    .then((user) => {
+      // Если объект по ID не найден
+      if (!user) {
+        res
+          .status(ERROR_NOT_FOUND)
+          .send({ message: `User with ID ${req.user._id} not found.` });
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      // Если ID не формата ObjectID
+      if (err.name === 'CastError') {
+        res.status(ERROR_WRONG_DATA).send({
+          message: `Unvalid id: ${err.message}`,
         });
-    } else {
-      res.status(ERROR_NOT_FOUND).send({
-        message: `User with Id ${req.user._id} not found`,
+      }
+      res.status(ERROR_ANOTHER).send({
+        message: `Error in update user info process: ${err.message}`,
       });
-    }
-  });
+    });
 };
