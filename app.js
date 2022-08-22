d15226822e6d10c77d6181c5dc8cb6a5b8a52c9b
Dmitriy Lvov <Dmitriy.Lvov@electroshield.ru>
2022-08-22 21:14:46 +0400

    autotest_2

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string(),
-    email: Joi.string().required(),
+    avatar: Joi.string().pattern(/https?:\/\/[www]?\.?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]+/i),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), createUser);
