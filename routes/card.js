c59ec2c66f68d4422a2a4a9cfbaed0c6d370bf29
Dmitriy Lvov <Dmitriy.Lvov@electroshield.ru>
2022-08-23 10:39:09 +0400

    autotest_finish

--- routes/card.js
+++ routes/card.js
@@ -14,7 +14,7 @@ cardRouter.get('/cards', getAllCards);
 cardRouter.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required(),
+    link: Joi.string().required().pattern(/https?:\/\/[www]?\.?[a-z0-9-._~:/?#[\]@!$&'()*+,;=]+/i),
   }),
 }), createCard);
 
